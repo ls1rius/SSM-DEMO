dfad1d91822cda25e45783bec987a8d4b7cd739b
ls1rius <865212941@qq.com>
2019-03-09 15:56:05 +0800

    SSM模板

---
+++ CrfPro/src/main/Java/com/cm/common/Test.java
@@ -0,0 +1,4 @@
+package com.cm.common;
+
+public class Test {
+}
---
+++ CrfPro/src/main/Java/com/cm/service/IUserService.java
@@ -0,0 +1,11 @@
+package com.cm.service;
+
+import com.cm.entity.User;
+
+public interface IUserService {
+
+    User selectUser(long userId);
+
+    int insert(User user);
+
+}
